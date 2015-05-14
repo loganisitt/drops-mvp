c3112717c8c1af38115b148fcd006ac3016a3d16
Vilcea <iosifvilcea@gmail.com>
2015-05-14 13:26:20 -0400

    Facebook & Google+ Integration added.

--- Android/app/src/main/java/com/drops/drops/MainActivity.java
+++ Android/app/src/main/java/com/drops/drops/MainActivity.java
@@ -1,17 +1,31 @@
 package com.drops.drops;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInBtn = (Button)findViewById(R.id.btn_signIn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
