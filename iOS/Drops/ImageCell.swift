b3b689dcfb29c1686cf6d2f9eaabfbb6c5968c50
Logan Isitt <laisitt@gmail.com>
2015-05-26 22:37:29 -0400

    Imported create view from SS master and client code from SS MEAN

---
+++ iOS/Drops/ImageCell.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCell.swift
+//  Drops
+//
+//  Created by Logan Isitt on 5/26/15.
+//  Copyright (c) 2015 Logan Isitt. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/Drops/Listing.swift
@@ -0,0 +1,9 @@
+//
+//  Listing.swift
+//  Drops
+//
+//  Created by Logan Isitt on 5/26/15.
+//  Copyright (c) 2015 Logan Isitt. All rights reserved.
+//
+
+import Foundation
