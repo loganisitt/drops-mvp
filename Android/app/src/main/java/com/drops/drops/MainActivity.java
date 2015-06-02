9d89cb5aa885ee3453485c62f287884032a2085a
Vilcea <iosifvilcea@gmail.com>
2015-06-02 13:59:34 -0400

    Basic Notifications

--- Android/app/src/main/java/com/drops/drops/MainActivity.java
+++ Android/app/src/main/java/com/drops/drops/MainActivity.java
@@ -1,30 +1,38 @@
 package com.drops.drops;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
+    Button raiseNotify;
 
-    // Google Cloud Messaging
+    // ** Notifications ***********************
+    private static final int NOTIFY_ME_ID = 1337;
+    private int count = 0;
+    private NotificationManager mgr = null;
+
+    // ** Google Cloud Messaging **************
     private GCMClientManager pushClientManager;
     String PROJECT_NUMBER = "357897084697";
-
-    Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ** Google Cloud Messaging ***********************************
+        // *************************************************************
         pushClientManager = new GCMClientManager(this, PROJECT_NUMBER);
         pushClientManager.registerIfNeeded(new GCMClientManager.RegistrationCompletedHandler() {
             @Override
@@ -33,31 +41,44 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        /*
-        // ******* LOGIN ACTIVITY *******
-        signInBtn = (Button)findViewById(R.id.btn_signIn);
-        signInBtn.setOnClickListener(new View.OnClickListener() {
+        // ** Notifications ********************************************
+        mgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        raiseNotify = (Button)findViewById(R.id.btn_Raise);
+        raiseNotify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+                // Create Intent for when Notification is clicked
+                //Intent resultIntent = new Intent(this, ACTIVITY_HERE);
+                //int requestID = (int) System.currentTimeMillis();
+                //int flags = PendingIntent.FLAG_CANCEL_CURRENT;
+                //PendingIntent pIntent = PendingIntent.getActivity(this, requestID, resultIntent,flags);
+
+                // CREATE A BASIC NOTIFICATION
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.notification_template_icon_bg)
+                        .setContentTitle("My notification!@")
+                        .setContentText("Hello m8!");
+                        //.setContentIntent(pIntent);
+
+                //Hide the notification after its selected.
+                mBuilder.setAutoCancel(true);
+
+                mgr.notify(NOTIFY_ME_ID, mBuilder.build());
             }
         });
-        */
-
-
-        // ******* END OF LOGIN ACTIVITY BUTTON *******
 
         // ******* BASIC LISTVIEW *******
-        String[] items = {"item1", "item2", "item3"};
-        ArrayAdapter<String> itemsAdapter
-                = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        //String[] items = {"item1", "item2", "item3"};
+        //ArrayAdapter<String> itemsAdapter
+        //        = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 
-        ListView itemLv = (ListView)findViewById(R.id.activity_list);
-        itemLv.setAdapter(itemsAdapter);
+        //ListView itemLv = (ListView)findViewById(R.id.activity_list);
+        //itemLv.setAdapter(itemsAdapter);
         // ******* END OF BASIC LISTVIEW *******
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
